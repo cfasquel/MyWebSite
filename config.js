fec0badfaa9141695d3c19ad7c436bde04cfcf75
Clement <fasquel.clement@gmail.com>
2020-01-05 22:53:36 +0100

    added gatsby template

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: 'Clément Fasquel, développeur web', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#228DFF',
+  manifestThemeColor: '#228DFF',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Clément',
+  lastName: 'Fasquel',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/cfasquel',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/clementfasquel/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/FasquelClement',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+  email: 'fasquel.clement@gmail.com',
+  phone: '+33631231781',
+  address: 'adress',
+};
